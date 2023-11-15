853130337a6c9ab0468e5d5b9f73091fa71cc3c0
Eloy Melo <eloym@Eloys-Mac-mini.local>
2023-11-15 13:42:10 -0300

    changes to the description

--- band-name-generator.py
+++ band-name-generator.py
@@ -1,5 +1,3 @@
-# this is the first challenge of the 100 days of code: the complete python bootcamp on Udemy.
-# in this simple piece of code I will create a name of a band combining the name of a pet and the name of a city.
 print("Welcome to the band name genarator!")
 city = input("What's the name of the city where you grew up in?\n")
 pet = input("What's the name of your pet?\n")
